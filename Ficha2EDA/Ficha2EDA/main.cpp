fb0eff34a9e5f53e09e60d5434b116373bed1062
Henrique <Henrique@COMPUTER>
2018-10-18 10:23:29 +0100

    Adicionar metodo iniciar,apagar, expandir ler

--- Ficha2EDA/Ficha2EDA/main.cpp
+++ Ficha2EDA/Ficha2EDA/main.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-	/*char filename[] = { "test.txt" };
+	char filename[] = { "test.txt" };
 	printf("%d",Matriz::Ler(filename));
-	system("pause");*/
+	system("pause");
 }
--- Ficha2EDA/Ficha2EDA/matriz.cpp
+++ Ficha2EDA/Ficha2EDA/matriz.cpp
@@ -2,13 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+Matriz::Matriz(int nLinhas, int nColunas)
+{
+	Iniciar(nlinhas, ncols);
+}
+
 Matriz::Matriz() { //construtor por omissão
 	elems = NULL;
 	nlinhas = 0;
 	ncols = 0;
 }
 
-Matriz::Matriz(int l, int c) {
+void Matriz::Apagar()
+{
+	for (int i = 0; i < nlinhas; i++) {
+		delete[] elems[i];
+		}
+	delete[] elems;
+	}
+
+
+void Matriz::Iniciar(int l, int c) {
 	nlinhas = l;
 	ncols = c;
 	elems = new float*[nlinhas];
@@ -23,14 +37,21 @@ Matriz::Matriz(int l, int c) {
 	}
 }
 
-bool Matriz::Ler(char * nome_ficheiro)
+bool Matriz::Ler(char *nome_ficheiro)
 {
 	FILE *ficheiro;
 	ficheiro = fopen(nome_ficheiro, "r");
 	if (ficheiro == NULL)
 		return false;
 	else {
-		
+		Apagar();
+		fscanf(ficheiro,"%d %d", nlinhas,ncols);
+		Iniciar(nlinhas, ncols);
+		for (int i = 0; i < nlinhas; i++) {
+			for (int j = 0; j > nlinhas; j++) { //inicialização
+				fscanf(ficheiro,"%f",elems[i][j]);
+			}
+		}
 		return true;
 	}
 }
